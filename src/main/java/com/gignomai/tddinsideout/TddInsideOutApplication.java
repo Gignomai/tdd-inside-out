73e7f32f4679c78818f0c824e43221d43b8ef90f
Manel Márquez <gignomaicsl@google.com>
2023-06-20 23:22:40 +0200

    Initialise repository

---
+++ src/main/java/com/gignomai/tddinsideout/TddInsideOutApplication.java
@@ -0,0 +1,13 @@
+package com.gignomai.tddinsideout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddInsideOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddInsideOutApplication.class, args);
+	}
+
+}
