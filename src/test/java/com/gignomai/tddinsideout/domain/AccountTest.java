f1b5c0296f68929c3effa8dacf44ce43925b89b8
Manel <gignomaicsl@gmail.com>
2023-06-21 18:24:22 +0200

    Some new tests and naming refactoring

--- src/test/java/com/gignomai/tddinsideout/domain/AccountTest.java
+++ src/test/java/com/gignomai/tddinsideout/domain/AccountTest.java
@@ -8,33 +8,40 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
-    public static final BigDecimal TEST_FUND_AMOUNT = BigDecimal.TEN;
+    public static final BigDecimal TEST_FUND_AMOUNT_TEN = BigDecimal.TEN;
 
     @Test
-    void shouldNotChangeTheAccountBalanceWhenFundingZeroToAnEmptyAccount() {
+    void balanceHasValueOfZeroWhenAccountIsNew() {
+        Account account = new Account();
+
+        assertThat(account.getBalance()).isZero();
+    }
+
+    @Test
+    void balanceHasValueOfZeroWhenAccountIsNewAndFundedZero() {
         Account account = new Account();
 
         account.fund(BigDecimal.ZERO);
 
-        assertThat(account.getBalance()).isEqualTo(BigDecimal.ZERO);
+        assertThat(account.getBalance()).isZero();
     }
 
     @Test
-    void shouldAddMoneyToAnEmptyAccountAndBalanceHasTheSameAmount() {
+    void balanceHasValueOfTenWhenAccountIsFundedTen() {
         Account account = new Account();
 
-        account.fund(TEST_FUND_AMOUNT);
+        account.fund(TEST_FUND_AMOUNT_TEN);
 
-        assertThat(account.getBalance()).isEqualTo(TEST_FUND_AMOUNT);
+        assertThat(account.getBalance()).isEqualTo(TEST_FUND_AMOUNT_TEN);
     }
 
     @Test
-    void shouldAddMoneyMultipleTimesToAnAccountAndBalanceEqualsTotalAdds() {
+    void balanceHasValueOfThirtyWhenAccountIsFoundedThreeTimesTen() {
         Account account = new Account();
 
-        account.fund(TEST_FUND_AMOUNT);
-        account.fund(TEST_FUND_AMOUNT);
-        account.fund(TEST_FUND_AMOUNT);
+        account.fund(TEST_FUND_AMOUNT_TEN);
+        account.fund(TEST_FUND_AMOUNT_TEN);
+        account.fund(TEST_FUND_AMOUNT_TEN);
 
         assertThat(account.getBalance()).isEqualTo(BigDecimal.valueOf(30L));
     }
