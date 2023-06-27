fb24625a8c85592b11b7422e8f6c46ac8ec7e249
Manel <gignomaicsl@gmail.com>
2023-06-28 00:04:23 +0200

    Added more tests for retrieving list of movements use cases.

--- src/main/java/com/gignomai/tddinsideout/domain/Account.java
+++ src/main/java/com/gignomai/tddinsideout/domain/Account.java
@@ -4,22 +4,29 @@ import lombok.Getter;
 
 import javax.naming.OperationNotSupportedException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 public class Account {
     private BigDecimal balance;
+    private final List<BigDecimal> movements;
 
     public Account() {
         balance = BigDecimal.ZERO;
+        movements = new ArrayList<>();
     }
 
     public Account(BigDecimal initialAmount) {
         balance = initialAmount;
+        movements = new ArrayList<>();
+        movements.add(initialAmount);
     }
 
     public void fund(BigDecimal amount) throws OperationNotSupportedException {
         if (amount.signum() > -1) {
             balance = balance.add(amount);
+            movements.add(amount);
         } else {
             throw new OperationNotSupportedException("Negative amounts can't be funded.");
         }
@@ -28,6 +35,7 @@ public class Account {
     public void withdraw(BigDecimal amount) throws OperationNotSupportedException {
         if (balance.compareTo(amount) >= 0) {
             balance = balance.subtract(amount);
+            movements.add(amount.negate());
         } else {
             throw new OperationNotSupportedException("Balance cannot be lower than withdraw amount.");
         }
--- src/test/java/com/gignomai/tddinsideout/domain/AccountTest.java
+++ src/test/java/com/gignomai/tddinsideout/domain/AccountTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import javax.naming.OperationNotSupportedException;
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -16,7 +17,7 @@ public class AccountTest {
 
         assertThat(account.getBalance()).isZero();
     }
-    
+
     @Test
     void balanceIsTenWhenAccountIsNewAndCreatedWithTen() {
         Account account = new Account(BigDecimal.TEN);
@@ -52,7 +53,7 @@ public class AccountTest {
     }
 
     @Test
-    void shouldThrowOperationNotSupportedExceptionWhenFundAmountIsNegative(){
+    void shouldThrowOperationNotSupportedExceptionWhenFundAmountIsNegative() {
         Account account = new Account();
 
         Throwable exception = assertThrows(OperationNotSupportedException.class, () -> account.fund(BigDecimal.valueOf(-1L)));
@@ -67,11 +68,11 @@ public class AccountTest {
 
         assertThat(account.getBalance()).isZero();
     }
-    
+
     @Test
     void balanceIsZeroWhenHavingTenWithdrawTen() throws OperationNotSupportedException {
         Account account = new Account(BigDecimal.TEN);
-        
+
         account.withdraw(BigDecimal.TEN);
 
         assertThat(account.getBalance()).isZero();
@@ -95,4 +96,50 @@ public class AccountTest {
         assertThat("Balance cannot be lower than withdraw amount.").isEqualTo(throwable.getMessage());
     }
 
+    @Test
+    void shouldNotReturnAccountMovementsIfThereIsNoActivity() {
+        Account account = new Account();
+
+        List<BigDecimal> movements = account.getMovements();
+
+        assertThat(movements.size()).isEqualTo(0);
+    }
+
+    @Test
+    void shouldReturnOneAccountMovementWithValueTenWhenAccountIsCreatedWithTen() {
+        Account account = new Account(BigDecimal.TEN);
+
+        List<BigDecimal> movements = account.getMovements();
+
+        assertThat(movements.size()).isEqualTo(1);
+        assertThat(movements.get(0)).isEqualTo(BigDecimal.TEN);
+    }
+
+    @Test
+    void shouldReturnTwoAccountMovementsWithValueTenWhenAccountIsEmptyAndFundedTenTwice() throws OperationNotSupportedException {
+        Account account = new Account();
+
+        account.fund(BigDecimal.TEN);
+        account.fund(BigDecimal.TEN);
+
+        List<BigDecimal> movements = account.getMovements();
+
+        assertThat(movements.size()).isEqualTo(2);
+        movements.forEach(m -> assertThat(m).isEqualTo(BigDecimal.TEN));
+    }
+
+    @Test
+    void shouldReturnTwoAccountMovementsWithTotalValueOfZeroWhenAccountCreatedWithTenAndWithdrawTen() throws OperationNotSupportedException {
+        Account account = new Account(BigDecimal.TEN);
+
+        account.withdraw(BigDecimal.TEN);
+
+        List<BigDecimal> movements = account.getMovements();
+
+        assertThat(movements.size()).isEqualTo(2);
+        BigDecimal total = movements.stream()
+                .reduce(BigDecimal::add)
+                .orElseThrow();
+        assertThat(total).isEqualTo(BigDecimal.ZERO);
+    }
 }
